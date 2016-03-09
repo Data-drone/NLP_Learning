38adb9e5f4a15459d1bd265d19fbfa9f6d7f106b
brian law <blaw@localhost.localdomain>
2016-03-10 00:35:41 +0800

    started looking at parsing bits

--- Exploration_NYTimes.R
+++ Exploration_NYTimes.R
@@ -11,11 +11,20 @@ news <- read_csv("../NYTimes_Data/NYTimesBlogTrain.csv")
 
 # Test
 output = annotateString(news$Abstract[1])
+
 # read in and then write out the parses
+output_dir = './parse/'
 i = 0
 for (row in news$Abstract) {
   output = annotateString(row)
-  saveRDS(output, paste0('./parse/', i, '_parse.rds') )
+  saveRDS(output, paste0(output_dir, i, '_parse.rds') )
   i= i + 1
 }
 
+# load first
+J1 <- readRDS(paste0(output_dir, '0_parse.rds'))
+J1000 <- readRDS(paste0(output_dir, '1000_parse.rds'))
+
+# lets access J! and get the nouns
+require(dplyr)
+J1$token %>% filter(POS %in% c('NN', 'NNP') )
